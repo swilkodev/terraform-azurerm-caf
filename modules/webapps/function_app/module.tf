c9173da6ab9c58ed64c1722062f597c9e363c00e
Scott <swilko35@gmail.com>
2021-07-23 16:43:37 +1000

    allow function name to be derived based on naming conventions

--- modules/webapps/function_app/module.tf
+++ modules/webapps/function_app/module.tf
@@ -1,16 +1,16 @@
 
-# resource "azurecaf_name" "plan" {
-#   name          = var.name
-#   resource_type = "azurerm_function_app"
-#   prefixes      = var.global_settings.prefixes
-#   random_length = var.global_settings.random_length
-#   clean_input   = true
-#   passthrough   = var.global_settings.passthrough
-#   use_slug      = var.global_settings.use_slug
-# }
+resource "azurecaf_name" "funcname" {
+  name          = var.name
+  resource_type = "azurerm_function_app"
+  prefixes      = var.global_settings.prefixes
+  random_length = var.global_settings.random_length
+  clean_input   = true
+  passthrough   = var.global_settings.passthrough
+  use_slug      = var.global_settings.use_slug
+}
 
 resource "azurerm_function_app" "function_app" {
-  name                       = var.name
+  name                       = azurecaf_name.funcname.result
   location                   = var.location
   resource_group_name        = var.resource_group_name
   app_service_plan_id        = var.app_service_plan_id
